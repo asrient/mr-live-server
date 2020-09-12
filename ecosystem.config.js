75d6a83480d0caa6100af6dd7454947afc6ab1a7
Asrient <asen182000@hotmail.com>
2020-09-12 10:51:48 +0530

    updated scheduler

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,5 +9,16 @@ module.exports = {
     env_production: {
       NODE_ENV: "production",
     }
+  },
+  {
+    name: "mr-scheduler",
+    script: 'scheduler.js',
+    watch: '.',
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
   }]
 };
--- scheduler.js
+++ scheduler.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const EventEmitter = require('events');
 const { api } = require('./shared.js');
 var redis = require('redis');
+var mongoose = require('mongoose');
+var { Message } = require('./models');
 
 var tunnel = redis.createClient(process.env.REDIS_URL);
+
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost:27017/mr', { useNewUrlParser: true });
 
 tunnel.on("error", function (error) {
     console.error('redis tunnel error:', error);
@@ -14,12 +18,20 @@ tunnel.on("connect", function () {
 });
 
 tunnel.on("pmessage", function (pattern, channel, message) {
-    var body = JSON.parse(message)
+    var { timeout, data } = JSON.parse(message)
     if (channel == 'schedule:skipto') {
-        //
+        var room_id= data.room_id;
+        console.log('scheduling skipto',room_id,timeout)
+        setTimeout(()=>{
+            api.post('skipto',{room_id},(status,data)=>{
+                console.log('result of scheduled skipto',status,data)
+            })
+        },timeout)
     }
-    else if (channel == 'schedule:heho') {
-        //
+    else if (channel == 'schedule:room.dissolve') {
+        Message.deleteMany({ room_id: data.room_id }, (err) => {
+            console.log('removed chats from room id: ' + data.room_id, err)
+        });
     }
 });
 
